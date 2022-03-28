f7af3b958b1a450c78699d4d7974f68543cd5663
GitProject <bootinho91@gmail.com>
2022-03-28 13:35:16 +0700

    file moi

---
+++ GitProject/GitProject/28-3/khaibaosudungbien.js
@@ -0,0 +1,27 @@
+/*let i = 10;
+let f = 20.5;
+let b = true;
+let s = 'Hà Nội'
+
+document.write('i =' + i);
+document.write('</br>');
+document.write('f =' + f);
+document.write('<br/>');
+document.write('b =' + b);
+document.write('<br/>');
+document.write('s =' + s); */
+
+/*let width = 20
+let height = 10
+let area = width * height;
+document.write('Area =' + area)*/
+
+/*let a = 10;
+let b = 2;
+
+prompt('nhập hai số');
+
+console.log(a % b);
+
+alert('a là bội số của b');
+*/
